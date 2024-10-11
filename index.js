fa71ac88d283e9e7cee9b2ed43d17eabaf22c9ee
Selina227 <selinac33@nycstudents.net>
2024-10-11 12:28:35 -0400

    e

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
+  box: document.getElementById("container-box"),
 };
 
-function changeColor() {
-  let button = document.querySelector("btn");
+function createCard() {
+  const newCard = {
+    text: DOMSelectors.text.value,
+  };
+  let form = document.querySelector("form");
   //listen for a click event
-  button.addEventListener("click", function (event) {
+  button.addEventListener("submit", function (event) {
     //prevents default behavior
     event.preventDefault();
     //logging the click event
