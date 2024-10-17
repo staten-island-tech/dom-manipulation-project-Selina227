b52f3b945f43e8d19b157fa22f9855faab671af3
Selina227 <selinac33@nycstudents.net>
2024-10-17 12:27:45 -0400

    e

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const DOMSelectors = {
-  button: document.getElementById("btn"),
-  text: document.querySelector("#text"),
+  text: document.querySelector("text"),
   box: document.getElementById("container-box"),
+  form: document.querySelector("form"),
   name: document.getElementById("name"),
   cost: document.getElementById("cost"),
 };
 
-function makeCard() {
-  const newCard = {
-    name: DOMSelectors.name.value,
-    cost: DOMSelectors.cost.value,
-  };
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const name = DOMSelectors.name.value;
+  const cost = DOMSelectors.cost.value;
 
-  injectIntoDOM();
+  const card = createObject(name, cost, "");
+  injectCard(card);
+  clearFields();
+  clickDelete();
+});
+
+function createObject(name, cost) {
+  return {
+    name: name,
+    cost: cost,
+  };
+}
+function injectCard() {}
+function clearFields() {
+  DOMSelectors.name.value = "";
+  DOMSelectors.cost.value = "";
 }
 
-function injectIntoDOM() {}
+function clickDelete() {}
