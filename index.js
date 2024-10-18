618705dd7712ffec65c118cc7bce7ad71e48521a
Selina227 <selinac33@nycstudents.net>
2024-10-18 11:23:18 -0400

    e

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const DOMSelectors = {
   text: document.querySelector("text"),
   box: document.getElementById("container-box"),
   form: document.querySelector("form"),
-  name: document.getElementById("name"),
-  cost: document.getElementById("cost"),
+  nam: document.getElementById("name"),
+  cos: document.getElementById("cost"),
 };
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const name = DOMSelectors.name.value;
-  const cost = DOMSelectors.cost.value;
+  const name = DOMSelectors.nam.value;
+  const cost = DOMSelectors.cos.value;
 
   const card = createObject(name, cost, "");
   injectCard(card);
@@ -23,7 +23,26 @@ function createObject(name, cost) {
     cost: cost,
   };
 }
-function injectCard() {}
+function injectCard() {
+  const cardHTML = `<div id ="container-box">
+    <div class="card">
+        <label id = "name" class="text">Object Name:</label>
+        <input type="text"/>
+        <button type ="submit">Submit</button>
+      </div>
+      <div class="card">
+        <label id ="cost" class="text">Object Cost:</label>
+        <input type="text"/>
+        <button type ="submit">Submit</button>
+      </div>
+      <div class="card">
+        <label id ="image" class="text">Object Image:</label>
+        <input type="text"/>
+        <button type ="submit">Submit</button>
+      </div>
+    </div>`;
+  DOMSelectors.form.insertAdjacentHTML("afterend", cardHTML);
+}
 function clearFields() {
   DOMSelectors.name.value = "";
   DOMSelectors.cost.value = "";
