37ed8359a4f756be594f43095d9b2010fe16c707
Selina227 <selinac33@nycstudents.net>
2024-10-19 23:01:26 -0400

    e

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
 const DOMSelectors = {
-  text: document.querySelector("text"),
   box: document.getElementById("container-box"),
   form: document.querySelector("form"),
   nam: document.getElementById("name"),
   cos: document.getElementById("cost"),
+  imag: document.getElementById("image"),
 };
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   const name = DOMSelectors.nam.value;
   const cost = DOMSelectors.cos.value;
+  const imageURL = DOMSelectors.imag.value;
 
-  const card = createObject(name, cost, "");
+  const card = createObject(name, imageURL, cost);
   injectCard(card);
   clearFields();
-  clickDelete();
+  removeCard();
 });
 
-function createObject(name, cost) {
+function createObject(name, imageURL, cost) {
   return {
     name: name,
+    imageURL: imageURL,
     cost: cost,
   };
 }
-function injectCard() {
-  const cardHTML = `<div id ="container-box">
-    <div class="card">
-        <label id = "name" class="text">Object Name:</label>
-        <input type="text"/>
-        <button type ="submit">Submit</button>
-      </div>
-      <div class="card">
-        <label id ="cost" class="text">Object Cost:</label>
-        <input type="text"/>
-        <button type ="submit">Submit</button>
-      </div>
-      <div class="card">
-        <label id ="image" class="text">Object Image:</label>
-        <input type="text"/>
-        <button type ="submit">Submit</button>
-      </div>
-    </div>`;
-  DOMSelectors.form.insertAdjacentHTML("afterend", cardHTML);
-}
-function clearFields() {
-  DOMSelectors.name.value = "";
-  DOMSelectors.cost.value = "";
+
+function injectCard(card) {
+  const cardHTML = `<div class="card">
+    <h2>${card.name}</h2>
+    <img class="pic" src="${card.imageURL}">
+    <h2>${card.cost}</h2>
+    <button class="delete-btn">Delete</button>
+  </div>`;
+
+  // Insert the card into the container
+  DOMSelectors.box.insertAdjacentHTML("beforeend", cardHTML);
 }
 
-function clickDelete() {}
+function clearFields() {
+  DOMSelectors.nam.value = "";
+  DOMSelectors.cos.value = "";
+  DOMSelectors.imag.value = "";
+}
+
+function removeCard(event) {
+  const deleteButtons = document.querySelectorAll(".delete-btn");
+  deleteButtons.forEach((btn) => {
+    btn.addEventListener("click", removeCard);
+  });
+  const card = event.target.parentElement;
+  card.remove();
+}
